3407e87a5026c7cbd664d46fac2f477040e7e64a
LeadHood <adrianwallin07@gmail.com>
2025-08-29 23:21:00 +0200

    Temporary commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace AStarRaylib
+﻿using System;
+using Raylib_cs;
+
+namespace AStarRaylib
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Raylib.InitWindow(800, 800, "ASTAR");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Update();
+
+                Draw();
+            }
+        }
+
+        static void Update()
+        {
+
+        }
+
+        static void Draw()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawText("Hello guy", 500, 500, 20, Color.Red);
+
+            Raylib.DrawRectangle(10, 10, 100, 100, Color.Brown);
+
+            Raylib.EndDrawing();
         }
     }
 }
---
+++ Tile.cs
@@ -0,0 +1,57 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace AStarRaylib
+{
+    enum TileType
+    {
+        Unopened, 
+        Opened, 
+        Closed, 
+        Obstacle
+    }
+
+    class Tile
+    {
+        public Vector2 Position { get; private set; }
+
+        public TileType Type { get; set; }
+
+        private int H = 0;
+
+        public int G { get; private set; } = 0;
+        public int F { get => G + H; }
+
+        public Tile Parent { get; set; }
+
+        public Tile(Vector2 position, TileType type)
+        {
+            Position = position;
+            Type = type;
+        }
+
+        public void CalculateValues(Vector2 endPos)
+        {
+            //G adds senders G + 14 if diagonal, 10 if straight
+            if (Parent == null)
+            {
+                G = 0;
+            }
+            else
+            {
+                
+                G = Parent.G + (int)Math.Abs(Vector2.Distance(Parent.Position, Position) * 10);
+            }
+
+            //Calculate H
+            int xDiff = (int)Mathf.Abs(endPos.X - Position.X);
+            int yDiff = (int)Mathf.Abs(endPos.Y - Position.Y);
+            H = xDiff * 10 + yDiff * 10;
+
+            //H = (int)Mathf.Floor(Mathf.Sqrt((xDiff * xDiff + yDiff * yDiff) * 10));
+        }
+    }
+}
