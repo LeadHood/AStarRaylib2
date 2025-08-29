131948aea6779a4c2fe75c5635cb2732d04efe4c
LeadHood <adrianwallin07@gmail.com>
2025-08-29 23:08:58 +0200

    Init finally

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AStarRaylib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
